d72b6de453b66858231d5777da6829acf1ac74c4
dee0518 <dehya-518@naver.com>
2022-12-19 12:34:28 +0900

    :lipstick: Login UI 완료

---
+++ src/components/common/InputForm.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+type EventHandler = (e: any) => void;
+
+type TLabelProps = {
+  htmlFor: string;
+  [key: string]: string;
+};
+
+type TInputProps = {
+  id: string;
+  type: string;
+  [key: string]: string | boolean | EventHandler;
+};
+
+type TInputFormProps = {
+  label: TLabelProps;
+  input: TInputProps;
+};
+
+const InputForm = ({ label, input }: TInputFormProps) => {
+  const { htmlFor, ...labelProps } = label;
+  return (
+    <div>
+      <label htmlFor={htmlFor} {...labelProps} />
+      <Input {...input} />
+    </div>
+  );
+};
+
+export default InputForm;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 13px;
+  border: 0;
+  border-radius: 4px;
+  background: ${({ theme }) => theme.color_gray_10};
+
+  input::placeholder {
+    color: ${({ theme }) => theme.color_gray_80};
+  }
+
+  input::-webkit-input-placeholder {
+    color: ${({ theme }) => theme.color_gray_80};
+  }
+
+  input:-ms-input-placeholder {
+    color: ${({ theme }) => theme.color_gray_80};
+  }
+`;
