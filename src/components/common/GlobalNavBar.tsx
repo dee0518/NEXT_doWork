f06dd5cc89350c5730b4258e2a00cc685e37be75
dee0518 <dehya-518@naver.com>
2022-12-20 21:22:26 +0900

    :lipstick: GNB UI 완료

--- src/components/common/GlobalNavBar.tsx
+++ src/components/common/GlobalNavBar.tsx
@@ -4,11 +4,15 @@ import Image from 'next/image';
 import BlindTitle from 'components/Common/BlindTitle';
 import styled from 'styled-components';
 import { flexbox } from 'styles/mixin';
-import { mediaQuery1024 } from 'styles/mediaQuery';
 import logo from 'images/common/logo.svg';
 import { MAIN, MYPAGE } from 'constants/navigation';
 
-const menuList = [
+type menuItem = {
+  id: string;
+  link: string;
+};
+
+const menuList: menuItem[] = [
   {
     id: 'schedule',
     link: MAIN,
@@ -46,51 +50,32 @@ const GlobalNavBar = () => {
 export default GlobalNavBar;
 
 const Header = styled.header`
-  ${flexbox('row', 'nowrap', 'center', 'center')}
+  ${flexbox('column', 'nowrap', 'center', 'center')}
   position: fixed;
   left: 0;
-  bottom: 0;
+  top: 0;
   z-index: 2;
-  width: 100%;
-  height: 70px;
-  border-radius: 0 70px 0 0;
-  ${({ theme }) => `background: linear-gradient(to right, ${theme.color_purple_60}, ${theme.color_purple_40})`};
-
-  ${mediaQuery1024} {
-    flex-direction: column;
-    top: 0;
-    width: 64px;
-    height: 100%;
-    border-radius: 0 0 40px 0;
-    ${({ theme }) => `background: linear-gradient(to bottom, ${theme.color_purple_60}, ${theme.color_purple_40})`};
-  }
+  width: 64px;
+  height: 100%;
+  border-radius: 0 0 40px 0;
+  ${({ theme }) => `background: linear-gradient(to bottom, ${theme.color_purple_60}, ${theme.color_purple_40})`};
 `;
 
 const H1 = styled.h1`
   position: absolute;
-  left: 5%;
-  top: 50%;
-  transform: translate3d(0, -50%, 0);
-  ${mediaQuery1024} {
-    left: 50%;
-    top: 30px;
-    transform: translate3d(-50%, 0, 0);
-  }
+  left: 50%;
+  top: 30px;
+  transform: translate3d(-50%, 0, 0);
 `;
 
 const LogoLink = styled(Link)`
   display: block;
-  width: 36px;
-  height: 36px;
+  width: 44px;
+  height: 44px;
 
   img {
     width: 100%;
     height: 100%;
-  }
-
-  ${mediaQuery1024} {
-    width: 50px;
-    height: 50px;
   }
 `;
 
@@ -102,22 +87,13 @@ const Nav = styled.nav`
 const Ul = styled.ul`
   width: 100%;
   height: 100%;
-  ${flexbox('row', 'nowrap', 'center', 'center')}
+  ${flexbox('column', 'nowrap', 'center', 'center')}
   gap: 10px;
-
-  ${mediaQuery1024} {
-    flex-direction: column;
-  }
 `;
 
 const Li = styled.li`
-  width: 64px;
-  height: 100%;
-
-  ${mediaQuery1024} {
-    width: 100%;
-    height: 64px;
-  }
+  width: 100%;
+  height: 64px;
 `;
 
 const NavLink = styled(Link)`
@@ -144,15 +120,14 @@ const NavLink = styled(Link)`
   &.activated::after {
     content: '';
     position: absolute;
-    top: -41px;
-    right: -2px;
+    top: -18px;
+    right: -1px;
     display: block;
-    width: 70px;
-    height: 150px;
+    width: 95%;
+    height: 100px;
     background: url(images/common/menu/menu_bg.svg) no-repeat;
     background-size: contain;
     background-position: right center;
-    transform: rotate(-90deg);
   }
 
   &.activated::before {
@@ -161,15 +136,5 @@ const NavLink = styled(Link)`
 
   &.mypage.activated::before {
     background-image: url(images/common/menu/mypage_on.svg);
-  }
-
-  ${mediaQuery1024} {
-    &.activated::after {
-      top: -18px;
-      right: -1px;
-      width: 95%;
-      height: 100px;
-      transform: none;
-    }
   }
 `;
