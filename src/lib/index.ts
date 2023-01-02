5f8e528f7125a9f0bc6e6506ce6c9cf248c0a0a2
dee0518 <dehya-518@naver.com>
2023-01-02 21:34:36 +0900

    :fire: 배포를 위해 일부 작업물 업로드

--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-const fetchData = async (url: string, options: any = {}): Promise<any> => {
-  const response = await fetch(url, options);
-  const json = await response.json();
-
-  return json;
-};
-
-export default fetchData;
