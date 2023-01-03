7c34768da5a23e26af0d630d8add3d23da427eaf
dee0518 <dehya-518@naver.com>
2023-01-03 14:49:36 +0900

    :mag: open graph 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,6 +32,13 @@ export default class MyDocument extends Document {
       <Html lang="ko">
         <Head>
           <meta charSet="utf-8" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://next-do-work.vercel.app" />
+          <meta property="og:title" content="doWork" />
+          <meta property="og:image" content="/images/common/og_doWork.png" />
+          <meta property="og:description" content="함께 일해보아요:)" />
+          <meta property="og:site_name" content="doWork" />
+          <meta property="og:locale" content="en_US" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&family=Poppins:wght@400;700&display=swap"
             rel="stylesheet"
