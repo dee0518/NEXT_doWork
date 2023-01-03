605ed4ce36896e47dddd99a9f894c795ca19c5b2
dee0518 <dehya-518@naver.com>
2023-01-03 15:05:15 +0900

    :mag: og image 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://next-do-work.vercel.app" />
           <meta property="og:title" content="doWork" />
-          <meta property="og:image" content="/images/common/og_doWork.png" />
+          <meta property="og:image" content="gs://dowork-bd9d9.appspot.com/images/siteInfo/og_doWork.png" />
           <meta property="og:description" content="함께 일해보아요:)" />
           <meta property="og:site_name" content="doWork" />
           <meta property="og:locale" content="en_US" />
